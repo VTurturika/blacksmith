aa43bc3531eae40011138bb3d837016f7e2b6929
Victor Turturika <v.turturika@gmail.com>
2018-05-24 13:44:02 +0300

    fix(api): add Access-Control-Allow-Methods header

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server.use(restify.plugins.queryParser());
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTION')
   next();
 });
 
